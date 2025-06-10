0cb4aa7569806a40f46adce85d79d02118fed6d8
JamesCDZ <jdraperc@gmail.com>
2025-06-11 00:15:13 +0100

    Update Overlay.jsx

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -120,7 +120,7 @@ export const Overlay = () => {
 
       {/* Main content */}
       <div className="max-w-2xl mx-auto px-4 pb-8">
-        <div className="bg-white rounded-lg shadow-lg p-2 min-h-[400px]">
+        <div className="bg-white rounded-lg shadow-lg p-2 md:p-6 min-h-[400px]">
           {renderStepContent()}
         </div>
       </div>
