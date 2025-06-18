4f963ffe21102a5f7ddbf257a9f3677eab7bca25
JamesCDZ <james@hawfinch.com>
2025-06-18 12:30:14 +0100

    Update PrivacySlide.jsx

--- src/components/slides/PrivacySlide.jsx
+++ src/components/slides/PrivacySlide.jsx
@@ -219,17 +219,17 @@ export const PrivacySlide = ({ onSubmit, onBack, formData }) => {
         marketingOptOut: marketingOptOut,
       };
       
-      // Trigger GTM leadFormSubmission event
-      // window.dataLayer = window.dataLayer || [];
-      // window.dataLayer.push({
-      //   event: 'leadFormSubmission',
-      //   firstName: finalFormData.firstName,
-      //   lastName: finalFormData.lastName,
-      //   email: finalFormData.email,
-      //   phone: finalFormData.phone,
-      //   address: `${finalFormData.street}, ${finalFormData.town}, ${finalFormData.postcode}`,
-      //   residentialStatus: 'Owner',
-      // });
+      Trigger GTM leadFormSubmission event
+      window.dataLayer = window.dataLayer || [];
+      window.dataLayer.push({
+        event: 'leadFormSubmission',
+        firstName: finalFormData.firstName,
+        lastName: finalFormData.lastName,
+        email: finalFormData.email,
+        phone: finalFormData.phone,
+        address: `${finalFormData.street}, ${finalFormData.town}, ${finalFormData.postcode}`,
+        residentialStatus: 'Owner',
+      });
       
       console.log('GTM leadFormSubmission event triggered');
       
